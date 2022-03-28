6b88fc58f8a1715ff72c96aa982c6f30c767d09d
akdelvalle <100975383+akdelvalle@users.noreply.github.com>
2022-03-28 14:48:48 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function menuToggle() {
     var x = document.getElementById ('myNavtoggle');
 
     if (x.className === 'navtoggle') {
-        x.className += 'responsive';
+        x.className += ' responsive';
     } else {
         x.className = 'navtoggle';
     }   
