6d8da64f117977eeaadfd29992b79cfbe7fba1a0
akdelvalle <100975383+akdelvalle@users.noreply.github.com>
2022-04-02 12:49:09 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 
-console.log ("Hello! Welcome to my Portfolio Site!")
+console.log("Hello! Welcome to my Portfolio Site!")
 
 function menuToggle() {
-    var x = document.getElementById ('myNavtoggle');
+    var x = document.getElementById('myNavtoggle');
 
     if (x.className === 'navtoggle') {
         x.className += ' responsive';
     } else {
         x.className = 'navtoggle';
-    }   
+    }
 }
